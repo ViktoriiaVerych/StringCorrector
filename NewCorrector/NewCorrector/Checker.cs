4eedc6cb3fce011173cad98885eead64738ead63
vs <santi.anjm@gmail.com>
2023-05-07 19:55:10 +0300

    Preparation task// Optimized naming// SuitableWords Added

---
+++ NewCorrector/NewCorrector/Checker.cs
@@ -0,0 +1,65 @@
+﻿namespace NewCorrector;
+
+public class Checker
+{
+    private readonly List<string> _data;
+    //constuctor
+    public Checker( List<string> data)
+    {
+        _data = data;
+    }
+    
+    //preparation task
+    public List<string> TyposChecking(IList<string> wordsList)
+    {
+        var mistakesInWords = new List<string>();
+        foreach (var word in wordsList)
+        {
+            if (!_data.Contains(word))
+            {
+                mistakesInWords.Add(word);
+            }
+        }
+
+        return mistakesInWords;
+    }
+    
+    public List<string> SuitableWords(List<string> wordsList)
+    {
+        var suitableWords = new List<string>();
+        foreach (var word in wordsList)
+        {
+            var counter = 0;
+            foreach (var word2 in _data)
+            {
+                var subLength = LCS(word, word2);
+                var levLength = Levenshtein(word, word2);
+                if (subLength == word.Length - 1 && levLength <= 2)
+                {
+                    suitableWords.Add(word2);
+                    counter++;
+                }
+    
+                if (counter == 5)
+                {
+                    break;
+                }
+            }
+        }
+    
+        return suitableWords;
+    }
+
+    private int LCS(string first, string second)
+    {
+        //later by Viktoriia
+    }
+
+
+    private int Levenshtein(string f, string s)
+    {
+        var firstWordLength = f.Length;
+        var secondWordLength = s.Length;
+        //later by Annechka
+    }
+}
--- NewCorrector/NewCorrector/Program.cs
+++ NewCorrector/NewCorrector/Program.cs
@@ -1,1 +1,67 @@
-﻿//checking1
+﻿using System.Collections;
+using NewCorrector;
+
+var data = new List<string>();
+
+foreach (var line in File.ReadAllLines(@"C:\Users\Admin\RiderProjects\NewCorrector\StringCorrector\NewCorrector\NewCorrector\WordList.txt"))
+{
+    data.Add(line);
+}
+
+var checker = new Checker(data);
+var separators = " .,-+:;!?";
+bool end = false;
+while (!end)
+{
+    Console.WriteLine("Enter a sentence: ");
+    string input = Console.ReadLine();
+    var words = input.Split(' ','.',',','—','+',':',';','!','?');
+    var wordsList = new List<string>();
+    foreach (var word in words)
+    {
+        wordsList.Add(word);
+    }
+
+    Fixer(wordsList);
+    var mistakes = checker.TyposChecking(wordsList);
+    Console.Write("Looks like you have typos in next words: ");
+    Console.WriteLine();
+    foreach (var word in mistakes)
+    {
+        Console.Write($"'{word}' ");
+    }
+
+    var result = checker.SuitableWords(mistakes);
+    Console.WriteLine("Suggested correction:");
+    Console.WriteLine();
+    foreach (var word in result)
+    {
+        Console.Write($"'{word}' ");
+    }
+    Console.WriteLine();
+    Console.Write("Continue: ");
+    if (Console.ReadLine() == "no") 
+        end = true;
+
+
+
+}
+
+void Fixer(IList<string> data)
+{
+    for (var i = 0; i < data.Count; i++)
+    {
+        foreach (var separator in separators)
+        {
+            if (data.Contains(separator.ToString()))
+            {
+                data.Remove(separator.ToString());
+            }
+
+            if (data.Contains(""))
+            {
+                data.Remove("");
+            }
+        }
+    }
+}
