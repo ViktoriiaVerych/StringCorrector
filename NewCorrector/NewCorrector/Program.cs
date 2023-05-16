36f41dbd64c4e609ebac8ae1eda1b12db1f39c7e
vs <santi.anjm@gmail.com>
2023-05-16 17:47:01 +0300

    Moded // Comments

--- NewCorrector/NewCorrector/Program.cs
+++ NewCorrector/NewCorrector/Program.cs
@@ -1,42 +1,48 @@
 ﻿
 public class Program
 {
-    // private static List<string> data;
+    
 
     public static void Main(string[] args)
+    //accepts command-line arguments as an array of strings args
     {
-        // data = new List<string>();
-        //
-        // foreach (var line in File.ReadAllLines(@"C:\Users\Admin\RiderProjects\NewCorrector\StringCorrector\NewCorrector\NewCorrector\WordList.txt"))
-        // {
-        //     data.Add(line);
-        // }
-
-        Console.WriteLine("Enter string s: ");
+        
+        Console.WriteLine("Enter string t: ");
         string t = Console.ReadLine();
 
-        Console.WriteLine("Enter string t: ");
+        Console.WriteLine("Enter string s: ");
         string s = Console.ReadLine();
 
         bool isSubsequence = IsSubsequence(s, t);
-        Console.WriteLine($"Is '{t}' a subsequence of '{s}'? {isSubsequence}");
+        Console.WriteLine($"Is '{s}' a subsequence of '{t}'? {isSubsequence}");
     }
-
+    //passes the strings s and t as arguments, to determine if s is a subsequence of t
+    //the result is stored in the boolean variable isSubsequence
     public static bool IsSubsequence(string s, string t)
     {
         int sIndex = 0;
         int tIndex = 0;
+        // two variables initialized sIndex and tIndex.
+        // these variables represent the current positions in the strings s and t, respectively
 
         while (sIndex < s.Length && tIndex < t.Length)
+        //the loop continues 'till there are characters left in both s and t    
         {
-            if (s[sIndex] == t[tIndex])
+            if (s[sIndex] == t[tIndex]) 
+            //checks if the character at the current position in s is equal to the character at the current position in t
+            
             {
                 sIndex++;
             }
 
             tIndex++;
+            //always incremented in each iteration to move to the next character in t
         }
 
         return sIndex == s.Length;
+        //checks if the entire s string has been processed
+        
+        //if sIndex is equal to s.Length, it means that all characters in s were found in t in the same order, and s is considered a subsequence of t
+        //the method returns true in this case; otherwise, it returns false
     }
 }
